97e4927fa776014a571872913d994006897d1af0
AmujoDotun <samuelamujo@gmail.com>
2020-04-08 01:19:34 +0100

    set the question page static page

---
+++ src/components/QuestionsBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+  <b-jumbotron>
+    <template v-slot:header>BootstrapVue</template>
+
+    <template v-slot:lead>
+      Question goes here
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+     List of answers comes here
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
