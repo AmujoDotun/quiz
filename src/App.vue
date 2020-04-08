9069d4b56da0c3b9bb26d330801cb217f73a4a1c
AmujoDotun <samuelamujo@gmail.com>
2020-04-08 01:20:21 +0100

    installed vue boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Header />
+   <b-row>
+     <b-col sm="6" offset="3">
+       <QuestionsBox />
+     </b-col>
+   </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionsBox from './components/QuestionsBox'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionsBox
   }
 }
 </script>
@@ -25,4 +31,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
